6a7fe0244717985cfa01437f6159a0f184c9b861
IgorZasypkin <zas.i.e@yandex.ru>
2020-11-14 02:08:21 +0300

    Tariffs.java

---
+++ src/Tariffs.java
@@ -0,0 +1,18 @@
+public class Tariffs {
+    int id;
+    String name;
+    int minute;
+    boolean ulimitedForTele2;
+    int internetLimit;
+    boolean unlimitedVk;
+    boolean unlimitedFaceBook;
+    boolean unlimitedOK;
+    boolean unlimitedWhatsApp;
+    boolean unlimitedWiber;
+    boolean unlimitedChat;
+    boolean unlimitedInstagramm;
+    boolean hitSales;
+    int price;
+    String buySim;
+    boolean optionsTariff;
+}
